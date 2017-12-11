0b56a4a587418f3ef383d84012e055d3b034495d
bastrakov.sergei <bastrakov.sergei@skbkontur.ru>
2017-12-11 10:04:24 +0500

    Add layouter which can move all rectangles relatively specified center

---
+++ homework/TagCloud.Core/Layouters/PositionedLayouter.cs
@@ -0,0 +1,22 @@
+﻿using TagCloud.Core.Layouters.Contract;
+using TagCloud.Core.Math;
+
+namespace TagCloud.Core.Layouters
+{
+    public class PositionedLayouter : IRectangleLayouter
+    {
+        private readonly IRectangleLayouter _rectangleLayouter;
+        private readonly Vector _center;
+
+        public PositionedLayouter(IRectangleLayouter rectangleLayouter, Vector center)
+        {
+            _rectangleLayouter = rectangleLayouter;
+            _center = center;
+        }
+
+        public Rectangle Place(Vector rectangleSize)
+        {
+            return _rectangleLayouter.Place(rectangleSize).Move(_center);
+        }
+    }
+}
