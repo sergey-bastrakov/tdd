2de89e850d67f2657e739508b281c5e1c948301b
bastrakov.sergei <bastrakov.sergei@skbkontur.ru>
2017-12-11 09:56:31 +0500

    Add circle shape factory

---
+++ homework/TagCloud.Core/Shapes/CircleShapeFactory.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections.Generic;
+using TagCloud.Core.Math;
+
+namespace TagCloud.Core.Shapes
+{
+    public class CircleShapeFactory
+    {
+        private readonly double _raduis;
+        private readonly int _segments;
+
+        public CircleShapeFactory(double raduis, int segments)
+        {
+            _raduis = raduis;
+            _segments = segments;
+        }
+
+        public IEnumerable<Vector> Create()
+        {
+            return new Circle(_raduis, _segments);
+        }
+    }
+}
